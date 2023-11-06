8d60d5c889fabd9aa79c594b1850b4a0049693b4
Muhammad Saroar <msaroar.dev@gmail.com>
2023-11-06 23:20:27 +0600

    added metadata to homepage and dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,10 @@
 import StatPage from "./stats/page";
 
+export const metadata = {
+  title: "Dashboard",
+  description: "NextAgency",
+};
+
 export default function Dashboard() {
   return (
     <>
